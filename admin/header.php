e9ae0ddff94fa32278516e1a8cf6dcc813000751
ken <darrelle.talisic19@gmail.com>
2024-04-25 08:50:14 +0800

    starting to use php

---
+++ admin/header.php
@@ -0,0 +1,101 @@
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/styles1.css">
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
+    <title>Dashboard-copy</title>
+</head>
+
+<body>
+    <section class="header">
+        <div class="logo">
+            <i class="ri-menu-line icon icon-0 menu"></i>
+            <h2>Cast<span>ello</span></h2>
+        </div>
+        <div class="search--notification--profile">
+            <div class="search">
+                <input type="text" placeholder="Search">
+                <button><i class="ri-search-2-line"></i></button>
+            </div>
+            <div class="notification--profile">
+                <div class="picon chat">
+                    <i class="ri-wechat-2-line"></i>
+                </div>
+                <div class="picon bell">
+                    <i class="ri-notification-2-line"></i>
+                </div>
+                
+                <div class="picon profile">
+                    <img src="images/profile.jpg" alt="profile" class="admin-pic">
+                </div>
+
+                <div class="account-name">admin</div>
+            </div>
+        </div>
+    </section>
+    <section class="main">
+        <div class="sidebar">
+            <ul class="sidebar--items">
+                <li>
+                    <a href="index.html" id="active--link">
+                        <span class="icon icon-1"><i class="ri-layout-grid-line"></i></span>
+                        <span class="sidebar--item">Dashboard</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="schedule.html">
+                        <span class="icon icon-2"><i class="ri-calendar-2-line"></i></span>
+                        <span class="sidebar--item">Schedule</span>
+                    </a>
+                
+                </li>
+                <li>
+                    <a href="doctors.html">
+                        <span class="icon icon-3"><i class="ri-user-2-line"></i></span>
+                        <span class="sidebar--item" style="white-space: nowrap;">Organization</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="patient.html">
+                        <span class="icon icon-4"><i class="ri-user-line"></i></span>
+                        <span class="sidebar--item">Patients</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <span class="icon icon-10"><i class="ri-medicine-bottle-fill"></i></span>
+                        <span class="sidebar--item">Pharmacy</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="inventory.html">
+                        <span class="icon icon-11"><i class="ri-file-list-2-fill"></i></span>
+                        <span class="sidebar--item">Inventory</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <span class="icon icon-5"><i class="ri-line-chart-line"></i></span>
+                        <span class="sidebar--item">Activity</span>
+                    </a>
+                </li>
+            </ul>
+            <ul class="sidebar--bottom-items">
+                <li>
+                    <a href="#">
+                        <span class="icon icon-7"><i class="ri-settings-3-line"></i></span>
+                        <span class="sidebar--item">Settings</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <span class="icon icon-8"><i class="ri-logout-box-r-line"></i></span>
+                        <span class="sidebar--item">Logout</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
---
+++ admin/index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include('header.php') ?>
+
+
+        <div class="main--content">
+            <div class="overview">
+                <div class="title">
+                    <h3 class="section--title">Overview</h3>
+                    
+                </div>
+                <div class="cards js-cards">
+
+                </div>
+            </div>
+            <div class="doctors">
+                <div class="title">
+                    <h3 class="section--title">Doctors</h3>
+                    <div class="doctors--right--btns">
+                        <select name="date" id="date" class="dropdown doctor--filter">
+                            <option value="all">All</option>
+                            <option value="onduty">On Duty</option>
+                            <option value="dayoff">Day Off</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="doctors--cards items"></div>
+            </div>
+            <div class="lowerpart-div">
+
+                <div class="graphs-div">
+                    <div class="patient-graph">
+                        <p class="patient-activites-word">Patient Activities</p>
+                        <canvas id="myChart">
+                        </canvas>
+                    </div>
+                    <div class="patient-donutchart">
+                        <canvas id="myDonutChart"></canvas>
+                    </div>
+                </div>
+               
+                <div class="table">
+                    <div class="title">
+                        <h3 class="section--title">Scheduled Patients</h3>            
+                    </div>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Date Scheduled</th>
+                                <th>Gender</th>
+                                <th>Age</th>
+                                <th>Settings</th>
+                            </tr>
+                        </thead>
+                        <tbody class="js-tables-scheduled-patients">
+                            
+                        </tbody>
+                    </table>
+                </div>
+                
+            </div>
+            
+        </div>
+    </section>
+    <script src="javascript/main.js"></script>
+    <script src="javascript/index.js" type="module"></script>
+</body>
+
+</html>
